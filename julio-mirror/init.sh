a4c2a9a595ef8e33bc12dde083cf3d00c2a74595
Brown, Bryan <bryjbrown@gmail.com>
2014-12-05 17:05:46 -0500

    Finished centos65-drupal7 boot w/ MySQL, added julio-mirror

--- julio-mirror/init.sh
+++ julio-mirror/init.sh
@@ -16,8 +16,8 @@ echo "AddHandler php5-script .php" >> /etc/httpd/conf/httpd.conf
 echo "AddType text/html .php" >> /etc/httpd/conf/httpd.conf
 cd /var/www
 rm -r html
-drush dl drupal-7.33
-mv drupal-7.33 html
+drush dl drupal-7.28
+mv drupal-7.28 html
 chmod -R 777 /var/www/html
 cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
 chmod 777 /var/www/html/sites/default/settings.php
@@ -35,3 +35,40 @@ mysql -e "CREATE USER 'drupal'@'localhost' IDENTIFIED BY 'drupal';"
 mysql -e "GRANT ALL PRIVILEGES ON drupal.* TO drupal@localhost;"
 mysql -e "FLUSH PRIVILEGES;"
 service httpd restart
+
+# Install modules with specifc version numbers to reflect Julio site
+drush dl block_titlelink-7.x-1.3
+
+<<COM
+calendar: 7.x-3.4
+captcha: 7.x-1.0
+cas: 7.x-1.3
+css_injector: 7.x-1.8
+custom_search: 7.x-1.12
+devel: 7.x-1.3
+ds: 7.x-2.4
+email_sender_override: 7.x (this module is tiny and probably not neccessary)
+fancy_login: 7.x-3.0-beta6
+field_permissions: 7.x-1.0-beta2
+google_analytics: 7.x-1.3
+htmlpurifier: 7.x-1.0
+jquery_plugin: 7.x-1.0
+js_injector: 7.x-2.0
+masquerade: 7.x-1.0-rc7
+menu_attributes: 7.x-1.0-rc2
+menu_block: 7.x-2.3
+menu_token: 7.x-1.0-beta5
+nice_menus: 7.x-2.4
+pathologic: 7.x-2.11
+rules: 7.x-2.6
+views_slideshow: 7.x-3.1
+webform: 7.x-3.20
+webform_report: 7.x-1.0
+webform_rules: 7.x-1.6
+
+LIBS:
+CAS (phpCAS): 1.3.2
+FirePHPCore: 0.3.1
+htmlpurifier: 4.5.0
+jquery.cycle: 3.0.3
+COM
