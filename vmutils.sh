8119ec4819fe9856bf09bb95552ea12ba8678e68
Brown, Bryan <bryjbrown@gmail.com>
2014-12-05 11:24:36 -0500

    Updated vmutils.sh and Vagrant machine names

---
+++ vmutils.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+CMD=$1
+
+if [ "$1" == "" ]
+then
+  echo "Missing argument."
+  exit
+fi
+
+case $1 in
+  new)
+    BASEVM=$2
+    NEWVM=$3
+    if [ -d $BASEVM ]; then
+      cp -r $BASEVM $NEWVM
+      perl -pi -e "s/\'$BASEVM\'/\'$NEWVM\'/" $NEWVM/Vagrantfile
+      vi $NEWVM/init.sh
+    else
+      echo "ERROR: Base VM '$BASEVM' does not exist." 
+    fi
+    ;;
+  check)
+    for VM in `ls`
+    do
+      if [ -f ./$VM/Vagrantfile ]
+      then
+        cd $VM; vagrant status | sed -n '3p'; cd ..
+      fi
+    done
+    ;;
+  *)
+    echo "Unknown argument."
+    ;;
+esac
