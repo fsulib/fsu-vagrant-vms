54242e2ffcb45ace7332a1f98b40e34917cdcdd7
Bryan Brown <bryjbrown@gmail.com>
2015-03-13 14:08:01 -0400

    Updated docker-host/init.sh so that docker is running correctly on boot

--- docker-host/init.sh
+++ docker-host/init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # Install core utils
-yum -y install epel-release
 yum -y install vim
 yum -y install git 
 yum -y install tmux
 
 # Install docker utils
+yum update -y device-mapper-libs
+yum -y install epel-release
 yum -y install docker-io
+service docker start
